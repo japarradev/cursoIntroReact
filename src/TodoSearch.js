10f7ad38bbfd58eb3e781e867b3efd79230d5a51
japarra <j.parra.903@gmail.com>
2023-05-22 18:50:40 -0500

    Prueba 1

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -3,15 +3,20 @@ import './TodoSearch.css';
 
 function TodoSearch() {
 
+  const [searchValue, SetSearchValue] = React.useState('');
+
   const onSearchValueSearch = (event)=>{
     console.log(event.target.value)
+    SetSearchValue(event.target.value)
   }
 
-  return (
+  return [
     <input className="TodoSearch" placeholder="Cebolla" 
     onChange={onSearchValueSearch}
-    />
-  );
+    value={searchValue}
+    />,
+    <p>{searchValue}</p>
+  ];
 }
 
 export { TodoSearch };
